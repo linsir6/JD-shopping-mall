f25fe62e9ef049247ff7b127f659495046dcf234
linsir <linsir678@gmail.com>
2018-01-29 14:48:22 +0800

    🚧 工作过程中: 替换掉原来的网络框架内容，重新编写网络框架

---
+++ app/src/main/java/top/linsir/jd_shopping_mall/application/App.kt
@@ -0,0 +1,14 @@
+package top.linsir.jd_shopping_mall.application
+
+import android.app.Application
+
+/**
+ *  Created by linSir
+ *  date at 2018/1/29.
+ *  describe:
+ */
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- app/src/main/java/top/linsir/jd_shopping_mall/model/HomeModelImpl.kt
+++ app/src/main/java/top/linsir/jd_shopping_mall/model/HomeModelImpl.kt
@@ -1,13 +1,9 @@
 package top.linsir.jd_shopping_mall.model
 
-import android.provider.Contacts
 import kotlinx.coroutines.experimental.Deferred
-import kotlinx.coroutines.experimental.android.UI
-import top.linsir.jd_shopping_mall.presenter.HomePresenter
-import kotlinx.coroutines.experimental.async
-import top.linsir.jd_shopping_mall.cancelByActive
 import top.linsir.jd_shopping_mall.delegate.LoginResponse
-import top.linsir.jd_shopping_mall.tryCatch
+import top.linsir.jd_shopping_mall.presenter.HomePresenter
+
 /**
  *  Created by linSir
  *  date at 2018/1/25.
@@ -22,21 +18,7 @@ class HomeModelImpl : HomeModel, CollectArticleModel {
             onLoginListener: HomePresenter.OnLoginListener,
             username: String, password: String
     ) {
-        async(UI) {
-            tryCatch({
-                it.printStackTrace()
-                onLoginListener.loginFailed(it.toString())
-            }) {
-                loginAsync?.cancelByActive()
-                loginAsync = RetrofitHelper.retrofitService.loginWanAndroid(username, password)
-                val result = loginAsync?.await()
-                result ?: let {
-                    onLoginListener.loginFailed(Constant.RESULT_NULL)
-                    return@async
-                }
-                onLoginListener.loginSuccess(result)
-            }
-        }
+        TODO("网络请求")
     }
 
     override fun collectArticle(onCollectArticleListener: HomePresenter.OnLoginListener, id: Int, isAdd: Boolean) {
