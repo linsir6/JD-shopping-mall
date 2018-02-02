9ea41a8ac43dbc7161c6e25fe2842bc632ce788b
linsir <linsir678@gmail.com>
2018-02-02 17:27:58 +0800

    🚧 工作过程中: 增加全局application

--- app/src/main/java/top/linsir/mall/application/App.kt
+++ app/src/main/java/top/linsir/mall/application/App.kt
@@ -1,6 +1,7 @@
 package top.linsir.mall.application
 
 import android.app.Application
+import android.content.Context
 import top.linsir.mall.network.ApiClient
 
 /**
@@ -8,9 +9,30 @@ import top.linsir.mall.network.ApiClient
  *  date at 2018/1/29.
  *  describe:
  */
-class App : Application() {
+public class App : Application() {
+
+
+
+    init {
+        instance = this;
+    }
+
+    companion object {
+        private var instance: Application? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
     override fun onCreate() {
         super.onCreate()
         ApiClient.instance.init()
+        val context: Context = App.applicationContext()
     }
+
+    public fun getContext(): Context {
+        return applicationContext;
+    }
+
 }
